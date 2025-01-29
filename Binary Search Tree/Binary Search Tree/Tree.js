f7cbd6bdbc02dae5c493e3abeb33c834d7238998
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-01-29 12:02:05 -0300

    Update Tree.js

--- Binary Search Tree/Binary Search Tree/Tree.js
+++ Binary Search Tree/Binary Search Tree/Tree.js
@@ -1,18 +1,31 @@
 class Tree {
     constructor() {
-        this.root=null;
+        this.root = null;
     }
 
-    addNode(node){
-        this.node= node;
-        if(this.root== null){
-            this.root=this.node;   
+    addNode(node) {
+        if (this.root === null) {
+            this.root = node; 
+        } else {
+            this.aux(this.root, node);
         }
-        this.root.left =node;
-        this.root.right =node;
+    }
 
+    aux(parent, child){
+            if(parent.data> child.data){
+                if(parent.left === null){
+                    parent.left =child;
+                }else{
+                    this.aux(parent.left, child);
+                }
+            }else{
+                if(parent.right ===null){
+                    parent.right = child;
+                }else{
+                    this.aux(parent.right, child);
+                }
+            }
     }
 }
-
 
 module.exports = Tree;
