a7601af30be5cfa9892f306031e691fd767c4657
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-01-30 09:34:24 -0300

    Update index.js

--- Binary Search Tree/Merkle Tree/index.js
+++ Binary Search Tree/Merkle Tree/index.js
@@ -1,1 +1,13 @@
 
+class MerkleTree {
+    constructor(leaves, concat) {
+        this.leaves = leaves;
+        this.concat= concat;
+    }
+    getRoot() {
+        return this.concat(this.leaves[0] , this.leaves[1]);
+    }
+
+}
+
+module.exports = MerkleTree;
