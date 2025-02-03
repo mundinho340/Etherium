9a75ceb845269c5bdc597c53e8918cb2aebacda2
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-03 14:12:34 -0300

    Update index.js

--- Binary Search Tree/Merkle Tree/index.js
+++ Binary Search Tree/Merkle Tree/index.js
@@ -1,13 +1,26 @@
-
 class MerkleTree {
     constructor(leaves, concat) {
         this.leaves = leaves;
-        this.concat= concat;
-    }
-    getRoot() {
-        return this.concat(this.leaves[0] , this.leaves[1]);
+        this.concat = concat;
     }
 
+    getRoot() {
+        let layer = this.leaves;
+        
+        while (layer.length > 1) {
+            let nextLayer = [];
+            for (let i = 0; i < layer.length; i += 2) {
+                if (i + 1 < layer.length) {
+                    nextLayer.push(this.concat(layer[i], layer[i + 1]));
+                } else {
+                    nextLayer.push(layer[i]);
+                }
+            }
+            layer = nextLayer;
+        }
+        
+        return layer[0];
+    }
 }
 
 module.exports = MerkleTree;
