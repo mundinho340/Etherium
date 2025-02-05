d7b9df913f54fda580a88484ebabafeb55a2c0a2
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-05 12:45:14 -0300

    Update Trie.js

--- Blockchain Storage-1/Trie.js
+++ Blockchain Storage-1/Trie.js
@@ -2,7 +2,25 @@ const TrieNode = require('./TrieNode');
 
 class Trie {
     constructor() {
-        this.root = new TrieNode();
+        this.root = new TrieNode(null);
+    }
+
+    insert(data){
+        let currentNode = this.root;
+        const chars = data.split("");
+
+        for(let i =0; i< chars.length; i++){
+            const char = chars[i]
+            if(!currentNode.children[char]){
+                currentNode.children[char] = new TrieNode(char);
+            }
+
+            currentNode = currentNode.children[char];
+
+            if(i === chars.length -1){
+                currentNode.isWord = true;
+            }
+        }
     }
 }
 
