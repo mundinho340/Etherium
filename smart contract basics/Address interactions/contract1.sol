bfdee96da42cef8968d5b432036f5b06abf27391
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-14 21:22:48 +0200

    Update contract1.sol

--- smart contract basics/Address interactions/contract1.sol
+++ smart contract basics/Address interactions/contract1.sol
@@ -1,9 +1,10 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 contract Contract {
     address public owner;
-    construtor(){
+    constructor(){
         owner =msg.sender;
     }
 }
