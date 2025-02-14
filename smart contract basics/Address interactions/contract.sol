9417637b9aa70a3511711e66350ecfb6699ba26a
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-14 21:48:08 +0200

    Update contract.sol

--- smart contract basics/Address interactions/contract.sol
+++ smart contract basics/Address interactions/contract.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
+contract Contract {
+    address public owner;
+    constructor(){
+        owner =msg.sender;
+    }
+
+    function tip() external payable {
+        (bool s,) = owner.call{value: msg.value}("");
+        require(s);
+    }
+
+    receive() payable external {
+        
+    }
+}
