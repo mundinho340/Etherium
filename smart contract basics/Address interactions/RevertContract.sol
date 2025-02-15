31210770656bddad7c628906186b1497978b6499
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-15 20:56:40 +0200

    Update RevertContract.sol

--- smart contract basics/Address interactions/RevertContract.sol
+++ smart contract basics/Address interactions/RevertContract.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
+contract Contract {
+    constructor() payable{
+        if(address(this).balance< 1e18){
+            revert("Erro");
+        }
+    }
+}
