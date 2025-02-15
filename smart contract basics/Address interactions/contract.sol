4b4496f0d17a66e657bf501f7188402a0e3ce062
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-15 16:20:52 +0200

    Update contract.sol

--- smart contract basics/Address interactions/contract.sol
+++ smart contract basics/Address interactions/contract.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.4;
 
 contract Contract {
     address public owner;
-    constructor(){
+    address public charity;
+    constructor(address _charity){
         owner =msg.sender;
+        charity = _charity;
+        
     }
 
     function tip() external payable {
@@ -12,7 +15,14 @@ contract Contract {
         require(s);
     }
 
+    function donate () external {
+        charity.call{value: address(this).balance}("");
+
+        selfdestruct(payable(msg.sender));
+    }
+
     receive() payable external {
         
     }
 }
+
