b787b5c93770d0a8049ff1d527c543e156ef9a4a
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-15 21:16:13 +0200

    Update RevertContract.sol

--- smart contract basics/Address interactions/RevertContract.sol
+++ smart contract basics/Address interactions/RevertContract.sol
@@ -2,9 +2,22 @@
 pragma solidity ^0.8.4;
 
 contract Contract {
+    address public owner;
+    modifier onlyowner{
+        require(msg.sender == owner);
+        _;
+    }
     constructor() payable{
+        owner = msg.sender;
         if(address(this).balance< 1e18){
             revert("Erro");
         }
+    }
+
+    function withdraw() external onlyowner{
+        //owner(this).call{value: msg.balance}("");
+       // uint balance = address(this).balance;
+        (bool sucess,)= payable(owner).call{value: address(this).balance}("");
+        require(sucess);
     }
 }
