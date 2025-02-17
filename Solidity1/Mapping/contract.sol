0118258aedc756e87711914ba120c719639f4db6
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-17 19:45:49 +0200

    Create contract.sol

---
+++ Solidity1/Mapping/contract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Contract {
+    mapping(address => bool) public members;
+    function addMember(address _member) external {
+        members[_member] = true;
+    }
+}
