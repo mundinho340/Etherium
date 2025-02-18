f09e94364dfdacb271ab1ddf3eef0a056e3b3082
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-18 20:51:47 +0200

    Update Party.sol

--- Solidity Core/Solidity Challenge/Party.sol
+++ Solidity Core/Solidity Challenge/Party.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
+contract Party {
+    uint public value;
+    mapping(address => bool) members;
+	constructor(uint val){
+        value = val;
+    }
+
+    function rsvp() external payable{
+         require(msg.value == value, "Valor insuficiente");
+         require(!members[msg.sender], "endereco ja validado");
+
+         members[msg.sender] =true;
+
+    }
+}
