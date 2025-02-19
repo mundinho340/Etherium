f7faf75966c2fa3af1c449ce056065d85bd1cae9
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-19 19:06:36 +0200

    Update Token.sol

--- Solidity Core/E-20/Token.sol
+++ Solidity Core/E-20/Token.sol
@@ -3,7 +3,11 @@ pragma solidity 0.8.4;
 
 contract Token {
     uint public totalSupply;
-    string public name = "Moeda Electronica Descentralizada";
+    string public name = "Moeda Electronica Descentralizada Exclusive";
     string public symbol ="MEDX";
     uint8 public decimals = 18;
+    mapping(address => uint) public balance;
+    function balanceOf(address _address) external view returns(uint){
+        return balance[_address];
+    }
 }
