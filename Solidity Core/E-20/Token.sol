1d020f094055e85c35bbbfcdab189601ac21694e
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-19 18:41:09 +0200

    Create Token.sol

---
+++ Solidity Core/E-20/Token.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+contract Token {
+    uint public totalSupply;
+    string public name = "Moeda Electronica Descentralizada";
+    string public symbol ="MEDX";
+    uint8 public decimals = 18;
+}
