67ea3e32f9649874290bcbef6cf3e6fb7beb1caa
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-20 17:37:08 +0200

    Update topics.js

--- Solidity Core/E-20/topics.js
+++ Solidity Core/E-20/topics.js
@@ -2,15 +2,14 @@ const { keccak256 } = require("ethereum-cryptography/keccak");
 const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
 
 function firstTopic() {
-    const eventSignature = "Transfer(address,address,uint256)"; 
+    const eventSignature = "Transfer(address,address,uint256)";
     const bytes = utf8ToBytes(eventSignature);
     const digest = keccak256(bytes);
     return toHex(digest);
 }
 
 function secondTopic() {
-    
-    const address = ""; 
+    return "00000000000000000000000028c6c06298d514db089934071355e5743bf21d60";
 }
 
-module.exports = { firstTopic, secondTopic }
+module.exports = { firstTopic, secondTopic };
