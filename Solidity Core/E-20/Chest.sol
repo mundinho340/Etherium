b4ac33d856c2b00e6ec7a60bd1be42d4dd1b9b6a
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-20 16:38:13 +0200

    Update Chest.sol

--- Solidity Core/E-20/Chest.sol
+++ Solidity Core/E-20/Chest.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "./IE20.sol";
+import "./IERC20.sol";
 
 contract Chest {
-    function plunder(address[] memory users) external {
-        for(uint8 i=0; i< users.length; i++){
-            msg.sender.call{value: users[i].balance};
+    function plunder(address [] memory users) external {
+        for(uint i=0; i< users.length; i++){
+            IERC20 token = IERC20(users[i]);
+            uint balance = token.balanceOf(address(this));
+
+            require(balance > 0, "Erro saldo insuficiente");
+            token.transfer(msg.sender, balance);
         }
     }
 }
