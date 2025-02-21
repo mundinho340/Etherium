30c796c08d32730f4c88a05607fb0095ecd0ed73
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-21 17:23:23 +0200

    Update TotalDaiTransferred.js

--- Solidity Core/E-20/TotalDaiTransferred.js
+++ Solidity Core/E-20/TotalDaiTransferred.js
@@ -1,25 +1,9 @@
 require("dotenv").config();
-const { firstTopic, secondTopic } = require('./topics');
-const topics = [firstTopic(), secondTopic()].map((x) => '0x' + x);
+const { firstTopic, secondTopic} = require("./topics");
+const topics =[firstTopic(), secondTopic()].map((x) => "0x" +x);
 
-const config = {
+const config ={
     apiKey: process.env.API_KEY,
     network: Network.ETH_MAINNET,
 };
 
-const alchemy = new Alchemy(config);
-
-async function totalDaiTransferred(fromBlock, toBlock) {
-    const logs = await alchemy.core.getLogs({
-        address: "", 
-        fromBlock,
-        toBlock,
-        topics
-    });
-
-
-
-  
-}
-
-module.exports = totalDaiTransferred;
