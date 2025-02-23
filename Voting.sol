6019a76ffb538275c72c6b2356df6de9cb37922d
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-23 20:21:46 +0200

    Update Voting.sol

--- Voting.sol
+++ Voting.sol
@@ -8,5 +8,16 @@ contract Voting {
         uint yesCount;
         uint noCount;
     }
+    Proposal [] public proposals;
+    function newProposal(address target, bytes memory data) external {
+        Proposal memory proposal;
+        proposal.target= target;
+        proposal.data=data;
+        proposal.yesCount=0;
+        proposal.noCount=0;
+        proposals.push(proposal);
+
+
+    }
     
 }
