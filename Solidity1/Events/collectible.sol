b3cafa4c14873ea79e0241308eaa2d7274a467bf
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-28 10:23:39 +0200

    Update collectible.sol

--- Solidity1/Events/collectible.sol
+++ Solidity1/Events/collectible.sol
@@ -3,7 +3,19 @@ pragma solidity ^0.8.4;
 
 contract Collectible {
     event Deployed(address _add);
+    address owner;
+    event Transfer(address, address);
     constructor(){
+        owner = msg.sender;
         emit Deployed(msg.sender);
     }
+
+    function transfer(address recipient) external {
+        require(owner == msg.sender, "Sorry only the ownership should be modifier this function");
+        require(owner != recipient,"Erro");
+        emit Transfer(owner, recipient);
+        owner =recipient;
+    }
+
+
 }
