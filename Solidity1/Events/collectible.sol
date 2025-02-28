69eb5eebbee6174196912618436e7e10dcc3bb8c
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-02-28 10:30:15 +0200

    Update collectible.sol

--- Solidity1/Events/collectible.sol
+++ Solidity1/Events/collectible.sol
@@ -3,11 +3,17 @@ pragma solidity ^0.8.4;
 
 contract Collectible {
     event Deployed(address _add);
-    address owner;
+    event ForSale(uint price, uint currentBlock);
     event Transfer(address, address);
+    address owner;
     constructor(){
         owner = msg.sender;
         emit Deployed(msg.sender);
+    }
+
+    modifier onlyOwner{
+        require(owner ==msg.sender, "Erro meu camarada.");
+        _;
     }
 
     function transfer(address recipient) external {
@@ -16,6 +22,10 @@ contract Collectible {
         emit Transfer(owner, recipient);
         owner =recipient;
     }
+
+    function markPrice(uint price) external onlyOwner {
+        emit ForSale(price, block.timestamp);
+    } 
 
 
 }
